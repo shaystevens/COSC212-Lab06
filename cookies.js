b2caf0d48e2ac5e74338640b95177185dbd679a9
sstevens <stesh969@student.otago.ac.nz>
2021-07-28 11:59:52 +1200

    Lab6 tests

---
+++ cookies.js
@@ -0,0 +1,36 @@
+var Cookie = (function () {
+    var pub = {};
+
+    pub.set = function (name, value, hours) {
+        var date, expires;
+        if (hours) {
+            date = new Date(); date.setHours(date.getHours() + hours); expires = "; expires=" + date.toGMTString();
+        } else {
+            expires = "";
+        }
+        document.cookie = name + "=" + encodeURIComponent(value) + expires + "; path=/";
+    };
+
+    pub.get = function (name) {
+        var nameEq, cookies, cookie, i;
+        nameEq = name + "=";
+        cookies = document.cookie.split(";");
+        for (i = 0; i < cookies.length; i += 1) {
+            cookie = cookies[i].trim();
+            if (cookie.indexOf(nameEq) === 0) {
+                return decodeURIComponent(cookie.substring(nameEq.length, cookie.length));
+            }
+        }
+        return null;
+    };
+
+    pub.clear = function (name) {
+        pub.set(name, "", -1);
+    };
+
+    return pub;
+}());
+
+//Cookie.set("Tally", "Sixteen", 24);
+//console.log(Cookie.get("Name"));
+//Cookie.clear("Tally");
